0a4d4c436a74ecce7eced678838fd28260c38cf9
suhyeoonn <suhyeon1740@gmail.com>
2023-11-10 07:22:01 +0900

    ✨ 거래내역 페이지 추가

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,66 +1,30 @@
 <script lang="ts">
-	import { LocalStorage } from '$lib/LocalStorage';
-
-	enum accountType {
-		INPUT,
-		OUTPUT
-	}
-
-	let date = new Date().toISOString().substring(0, 10);
-	let type = accountType.INPUT;
-	let description = '';
-	let amount = 0;
-
-	const onSubmit = (e: Event) => {
-		e.preventDefault();
-
-		if (amount < 1) {
-			alert('금액을 입력하세요.');
-			return;
+	const history = [
+		{
+			date: '2022-11-09',
+			description: '내용',
+			amount: 10000
+		},
+		{
+			date: '2022-11-09',
+			description: '내용',
+			amount: 10000
+		},
+		{
+			date: '2022-11-09',
+			description: '내용',
+			amount: 10000
 		}
-
-		LocalStorage.save({ date, type, description, amount });
-	};
-
-	const setType = (_type: accountType) => {
-		type = _type;
-	};
+	];
 </script>
 
-<form class="flex flex-col gap-10" on:submit={onSubmit}>
-	<input type="date" bind:value={date} required class="input input-bordered input-lg" />
-	<div class="btn-group">
-		<button
-			type="button"
-			class="btn"
-			class:btn-active={type === accountType.INPUT}
-			on:click={() => setType(accountType.INPUT)}
-		>
-			들어온 돈
-		</button>
-		<button
-			type="button"
-			class="btn"
-			class:btn-active={type === accountType.OUTPUT}
-			on:click={() => setType(accountType.OUTPUT)}
-		>
-			나간 돈
-		</button>
-	</div>
-
-	<input
-		type="text"
-		placeholder="설명"
-		bind:value={description}
-		class="input input-bordered input-lg"
-	/>
-	<input
-		type="number"
-		placeholder="금액"
-		bind:value={amount}
-		required
-		class="input input-bordered input-lg"
-		min="0"
-	/>
-	<button class="btn btn-primary">입력하기</button>
-</form>
+<h1>거래 내역 페이지</h1>
+<ul>
+	{#each history as { date, description, amount }}
+		<li>
+			<span>{date}</span>
+			<span>{description}</span>
+			<span>{amount}원</span>
+		</li>
+	{/each}
+</ul>
